fdcdbab29ed1b92167ab24563401e0da5de9b682
Temp <temp@Temps-MacBook-Pro.local>
2025-07-21 22:52:31 +0530

    Update module reference for eks  cluster name

--- terraform.tf
+++ terraform.tf
@@ -23,10 +23,10 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_name]
     command     = "aws"
   }
 }
